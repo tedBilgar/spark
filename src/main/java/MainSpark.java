07b974c2115eb435411cff76f8dbeaaad1c0b1f8
Denis Urakanov <mcquicklyss@gmail.com>
2019-10-15 10:26:04 +0500

    Count words

--- src/main/java/MainSpark.java
+++ src/main/java/MainSpark.java
@@ -1,2 +1,29 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class MainSpark {
+    public static void main(String[] args) {
+        SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("JavaWordCount");
+        JavaSparkContext ctx = new JavaSparkContext(sparkConf);
+        JavaRDD<String> lines = ctx.textFile("src/main/resources/static/story");
+
+        JavaRDD<String> words
+                = lines.flatMap(s -> Arrays.asList(s.split(" ")).iterator());
+        JavaPairRDD<String, Integer> ones
+                = words.mapToPair(word -> new Tuple2<>(word, 1));
+        JavaPairRDD<String, Integer> counts
+                = ones.reduceByKey((Integer i1, Integer i2) -> i1 + i2);
+
+        List<Tuple2<String, Integer>> output = counts.collect();
+        for (Tuple2<?, ?> tuple : output) {
+            System.out.println(tuple._1() + ": " + tuple._2());
+        }
+        ctx.stop();
+    }
 }
